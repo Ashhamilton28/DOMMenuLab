b08602bef83f63fe703d693f7e738971550eca04
Ashley <Ashhamilton22888@gmail.com>
2023-02-21 14:15:55 -0500

    another commit

---
+++ index.js
@@ -0,0 +1,8 @@
+//Select and cache the <main>element in a variable named mainEl
+const mainEl = document.getElementById('main');
+
+//Set the background color of mainEl to the value stored in the --main-bgCSS custom property. Hint: Assign a string that uses the CSS var()function like this:
+// 'var(--main-bg)'
+
+mainEl.style.backgroundColor = '#4a4e4d';
+
